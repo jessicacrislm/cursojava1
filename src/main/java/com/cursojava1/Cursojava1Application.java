2ea21c93fc6263415a85f0e774cc576d20ddd565
Jessica Machado <jessica-cristina@ultracar.com.br>
2020-02-17 21:42:39 -0300

    commit 17/02/20

---
+++ src/main/java/com/cursojava1/Cursojava1Application.java
@@ -0,0 +1,13 @@
+package com.cursojava1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursojava1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursojava1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursojava1/Cursojava1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursojava1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursojava1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
